966bb2c81f09f4c6d05efc8c7d6e9f4c403872f5
Oscar Ramirez <oscar.ramirez@itti.digital>
2022-10-10 14:20:51 -0300

    Agregamos función para comprobar JWT, además se agrega detalle en la sección de .gitignore

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,16 @@
 const { io } = require('../index');
+const { comprobarJWT } = require('../helpers/jwt');
 
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    //PARA Visualizar el cliente de donde se conecto y cual es el token
+    //console.log(client.handshake.headers['x-token']);
+
+    const [valido, uid] = comprobarJWT(client.handshake.headers['x-token'])
+    console.log(valido ,uid);
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
